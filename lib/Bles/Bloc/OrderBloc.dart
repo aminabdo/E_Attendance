9ecdf3e954c06714a997ecefbdccc3fe116a0b1c
aminabdo43@gmail.com <@CAshu011223234151>
2021-03-14 16:54:49 +0200

    bloc

--- lib/Bles/Bloc/OrderBloc.dart
+++ lib/Bles/Bloc/OrderBloc.dart
@@ -1,13 +1,22 @@
 import 'package:qimma/Bles/ApiRoute.dart';
+import 'package:qimma/Bles/Model/Requests/AddProductTopdOrder.dart';
+import 'package:qimma/Bles/Model/Requests/AddToCartRequest.dart';
+import 'package:qimma/Bles/Model/Requests/AddpdOrderRequest.dart';
+import 'package:qimma/Bles/Model/Responses/add_pd_order/AddpdOrderResponse.dart';
+import 'package:qimma/Bles/Model/Responses/add_pd_order/MakeBillResponse.dart';
 import 'package:qimma/Bles/Model/Responses/order/AllpdOrderResponse.dart';
 import 'package:qimma/Bles/Model/Responses/order/SinglepdOrder.dart';
 import 'package:qimma/utils/base/BaseBloc.dart';
+import 'package:qimma/utils/base/BaseResponse.dart';
 import 'package:rxdart/rxdart.dart';
 
 class OrderBloc extends BaseBloc {
 
   BehaviorSubject<AllpdOrderResponse> _all_pd_order = BehaviorSubject<AllpdOrderResponse>();
   BehaviorSubject<SinglepdOrderResponse> _single_P_d_order = BehaviorSubject<SinglepdOrderResponse>();
+  BehaviorSubject<AddpdOrderResponse> _add_pd_order = BehaviorSubject<AddpdOrderResponse>();
+  BehaviorSubject<BaseResponse> _add_product_to_pd_order = BehaviorSubject<BaseResponse>();
+  BehaviorSubject<MakeBillResponse> _make_bill = BehaviorSubject<MakeBillResponse>();
 
   Future<AllpdOrderResponse> all_p_d_Order() async {
     _all_pd_order.value = AllpdOrderResponse();
@@ -37,6 +46,7 @@ class OrderBloc extends BaseBloc {
     _all_pd_order.value.loading = false ;
     return response;
   }
+
   Future<SinglepdOrderResponse> single_P_d_order(int orderID) async {
     _single_P_d_order.value = SinglepdOrderResponse();
     _single_P_d_order.value.loading = true ;
@@ -45,6 +55,51 @@ class OrderBloc extends BaseBloc {
     _single_P_d_order.value.loading = false ;
     return response;
   }
+
+
+  /// order scenario
+  // 1 -> add_P_d_order
+  // 2 -> add_product_to_P_d_order
+  // 3 -> make_bill
+
+
+  Future<AddpdOrderResponse> add_P_d_order(AddpdOrderRequest request) async {
+    _add_pd_order.value = AddpdOrderResponse();
+    _add_pd_order.value.loading = true ;
+    AddpdOrderResponse response = AddpdOrderResponse.fromMap((await repository.post(ApiRoutes.add_P_d_order() , request.toJson())).data);
+    _add_pd_order.value = response;
+    _add_pd_order.value.loading = false ;
+    return response;
+  }
+
+
+  Future<BaseResponse> add_product_to_P_d_order(AddProductTopdOrder request) async {
+    _add_product_to_pd_order.value = BaseResponse();
+    _add_product_to_pd_order.value.loading = true ;
+    BaseResponse response = null ;// BaseResponse.fromMap((await repository.post(ApiRoutes.add_P_d_order() , request.toJson())).data);
+    _add_product_to_pd_order.value = response;
+    _add_product_to_pd_order.value.loading = false ;
+    return response;
+  }
+
+
+  Future<MakeBillResponse> makeBill(int orderID , int discount_type,double discount , int tax1_type , int tax1, int tax2_type , int tax2 , double paid) async {
+    _make_bill.value = MakeBillResponse();
+    _make_bill.value.loading = true ;
+    MakeBillResponse response = MakeBillResponse.fromMap((await repository.get(ApiRoutes.make_bill(orderID, discount_type, discount, tax1_type, tax1, tax2_type, tax2, paid))).data);
+    _make_bill.value = response;
+    _make_bill.value.loading = false ;
+    return response;
+  }
+
+
+
+
+
+
+
+
+
 
 
   BehaviorSubject<AllpdOrderResponse> get s_login => _all_pd_order;
